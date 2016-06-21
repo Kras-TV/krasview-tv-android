fbd938524ee75bc1c56b130b61b071d07e937e26
Anya Kaygorodova <anutakay@gmail.com>
2016-06-21 23:36:53 +0700

    Форк репозитория VLC для андроид добавлен в процесс сборки

---
+++ compile.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+checkfail()
+{
+    if [ ! $? -eq 0 ];then
+        echo "$1"
+        exit 1
+    fi
+}
+
+if [ ! -d "vlc-android" ]; then
+    echo "VLC-android source not found, cloning"
+    git https://github.com/anutakay/vlc-android-for-krasview.git
+    checkfail "vlc source: git clone failed"
+fi
+
+cd vlc-android
+./compile.sh -l
+cd ..
+
